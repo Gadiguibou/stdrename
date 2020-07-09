23af859af6cd185423fed80c7c268f59b20d2e47
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-07-09 15:01:01 -0400

    Add working change_naming_convention()

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::error::Error;
 use std::fs;
+use std::path::Path;
+
+use inflector::Inflector;
 
 pub struct Config {
     current_dir: String,
@@ -15,10 +18,61 @@ impl Config {
         let current_dir = args[0].clone();
         let naming_convention = args[1].clone();
 
-        Ok(Config { current_dir, naming_convention })
+        Ok(Config {
+            current_dir,
+            naming_convention,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
+}
+
+pub fn change_naming_convention(
+    path_to_file: &Path,
+    new_naming_convention: &str,
+) -> Result<String, &'static str> {
+
+    let file_stem = match path_to_file.file_stem() {
+        Some(s_opt) => match s_opt.to_str() {
+            Some(s) => s,
+            None => return Err("couldn't convert file stem to valid Unicode"),
+        },
+        None => "",
+    };
+
+    let file_extension = match path_to_file.extension() {
+        Some(extension_opt) => match extension_opt.to_str() {
+            Some(extension) => extension,
+            None => return Err("couldn't convert file extension to valid Unicode"),
+        },
+        None => "",
+    };
+
+    match new_naming_convention {
+        "snake_case" => Ok([file_stem.to_snake_case(), file_extension.to_string()].join(".")),
+        _ => Err("naming convention not found"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn doesnt_rename_files_with_no_breaks() {
+        assert_eq!(
+            Ok("file.txt".to_string()),
+            change_naming_convention(Path::new("file.txt"), "snake_case")
+        );
+    }
+
+    #[test]
+    fn renames_files_to_snake_case() {
+        assert_eq!(
+            Ok("i_like_to_fly.txt".to_string()),
+            change_naming_convention(Path::new("iLikeToFly.txt"), "snake_case")
+        )
+    }
 }
